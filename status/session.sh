6dda5643bd455cb6cea09f3bd4b10940e705d400
Matt Stillwell <stillwell006@gmail.com>
2024-05-03 18:37:31 -0400

    :art: Improve tmux colors

--- status/session.sh
+++ status/session.sh
@@ -3,7 +3,7 @@ show_session() {
 
   index=$1
   icon=$(get_tmux_option "@catppuccin_session_icon" "")
-  color=$(get_tmux_option "@catppuccin_session_color" "#{?client_prefix,#ebcb8b,#D8DEE9}")
+  color=$(get_tmux_option "@catppuccin_session_color" "#{?client_prefix,#88c0d0,#a3be8c}")
   text=$(get_tmux_option "@catppuccin_session_text" "#S")
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
