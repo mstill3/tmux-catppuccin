b78a10e1474abb34e5cd510b8b6926073969129f
Matt Stillwell <stillwell006@gmail.com>
2024-05-03 13:08:54 -0400

    :art: Add more customizable color options

--- status/session.sh
+++ status/session.sh
@@ -3,7 +3,7 @@ show_session() {
 
   index=$1
   icon=$(get_tmux_option "@catppuccin_session_icon" "")
-  color=$(get_tmux_option "@catppuccin_session_color" "#{?client_prefix,$thm_red,$thm_green}")
+  color=$(get_tmux_option "@catppuccin_session_color" "#{?client_prefix,@catppuccin_session_active_color,@catppuccin_session_inactive_color}")
   text=$(get_tmux_option "@catppuccin_session_text" "#S")
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
